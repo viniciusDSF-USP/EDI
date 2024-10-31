156d84404201b534a6b1c68e9052ab00b2f57871
viniciusDSF-USP <159798117+viniciusDSF-USP@users.noreply.github.com>
2024-10-31 16:32:17 +0000

    iguais

---
+++ 11sem/ter/lib/lista.h
@@ -0,0 +1,31 @@
+typedef struct no No;
+
+typedef union {
+	int i;
+	float f;
+	char c;
+	char *s;
+} Atom;
+
+typedef union {
+	Atom atom;
+	No* sub;
+} Elem;
+
+struct no {
+	Elem info;
+	enum {t_int, t_float, t_char, t_string, t_sublist} tipo;
+	struct no *prox;
+};
+
+typedef struct {
+	No *ini;
+	No *fim;
+} ListaGen;
+
+ListaGen* criar();
+void push(ListaGen*, Elem, int);
+void append(ListaGen*, ListaGen*);
+int busca(ListaGen*, Elem, int);
+int profundidade(ListaGen*);
+int iguais(ListaGen*, ListaGen*);
